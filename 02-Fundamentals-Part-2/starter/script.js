b69bebc665914d975da3d0ec80fa86883de0971a
Venatik <stefan.trajkovski008@gmail.com>
2024-01-14 16:37:44 +0100

    new file:   02-Fundamentals-Part-2/Coding Challenges/Challenge 6/index.html 	new file:   02-Fundamentals-Part-2/Coding Challenges/Challenge 6/script.js 	modified:   02-Fundamentals-Part-2/starter/script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -99,3 +99,76 @@
 
 // console.log(yearsUntilRetirement(1989, "Stefan"));
 // console.log(yearsUntilRetirement(1970, "Mike"));
+
+
+// ============ Introduction to Arrays ============
+
+// const friend1 = "Mike";
+// const friend2 = "Bob";
+// const friend3 = "Pepe";
+
+// const friends = ["Mike", "Bob", "Pepe"];
+// console.log(friends);
+
+// // const years = new Array(1991, 1989, 1984, 2008, 2020);
+
+// console.log(friends[1]); // log index 1
+// console.log(friends.length); // log number of items in array, this is not zero-based like the index
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = "Jay";
+// console.log(friends);
+// // friends = ["bob", "alice"]; // Doesn't work.
+
+// const firstName = "Stefan";
+// const stefan = [firstName, "Trajkovski", 2024 - 1989, "Dev", friends];
+// console.log(stefan);
+// console.log(stefan.length);
+
+// // Exercise
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const years = [1990, 1969, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+// console.log(ages);
+
+// ============ Basic Array Operations (Methods) ============
+
+// Add elements to array
+const friends = ["Mike", "Bob", "Pepe"];
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements to array
+friends.pop(); // Last element
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First element
+console.log(friends);
+
+console.log(friends.indexOf("Bob"));
+console.log(friends.indexOf("Jay"));
+
+console.log(friends.includes("Stefan"));
+console.log(friends.includes("Bob"));
+
+if (friends.includes("Peter")) {
+    console.log("You have a friend called Peter.");
+} else {
+    console.log("You don't have a friend called Peter.");
+}
+
